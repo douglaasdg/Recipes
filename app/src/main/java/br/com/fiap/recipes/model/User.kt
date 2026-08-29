7e82b3932881ab40c7ec8d7d03ae7f7f2d6d76db
Douglas Cristian <164271155+douglaasdg@users.noreply.github.com>
2026-08-29 07:15:54 -0300

    Revert "Feat/navigation implementation"

--- app/src/main/java/br/com/fiap/recipes/model/User.kt
+++
@@ -1,8 +0,0 @@
-package br.com.fiap.recipes.model
-
-data class User(
-    val id: Int = 0,
-    val name: String = "",
-    val email: String = "",
-    val password: String = ""
-)
--- app/src/main/java/br/com/fiap/recipes/ui/theme/Theme.kt
+++ app/src/main/java/br/com/fiap/recipes/ui/theme/Theme.kt
@@ -11,37 +11,33 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
-    private val DarkColorScheme = darkColorScheme(
-        primary = DarkPrimary,
-        secondary = DarkSecondary,
-        tertiary = DarkTertiary,
-        background = DarkBackground,
-        surface = DarkSurface,
-        onPrimary = DarkOnPrimary,
-        onSecondary = DarkOnSecondary,
-        onTertiary = DarkOnTertiary,
-        onBackground = DarkOnBackground,
-        onSurface = DarkOnSurface
-    )
+private val DarkColorScheme = darkColorScheme(
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
+)
 
-    private val LightColorScheme = lightColorScheme(
-        primary = LightPrimary,
-        secondary = LightSecondary,
-        tertiary = LightTertiary,
-        background = LightBackground,
-        surface = LightSurface,
-        onPrimary = LightOnPrimary,
-        onSecondary = LightOnSecondary,
-        onTertiary = LightOnTertiary,
-        onBackground = LightOnBackground,
-        onSurface = LightOnSurface
+private val LightColorScheme = lightColorScheme(
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
 )
 
 @Composable
 fun RecipesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = false,
+    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
